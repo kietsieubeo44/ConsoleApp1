31571a5cc05cce6c8be29b5a8a572628cbebb7ef
kiet <kietsieubeo44@gmail.com>
2023-08-29 20:05:11 +0700

    Add project files.

---
+++ ConsoleApp1/Bai1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class ToaDo
+{
+	private double x;
+	private double y;
+	private string tenToaDo;
+
+	public ToaDo()
+	{
+		x = 0.0;
+		y = 0.0;
+		tenToaDo = "Origin";
+	}
+
+	public ToaDo(double x, double y, string tenToaDo)
+	{
+		this.x = x;
+		this.y = y;
+		this.tenToaDo = tenToaDo;
+	}
+
+	public double X
+	{
+		get { return x; }
+		set { x = value; }
+	}
+
+	public double Y
+	{
+		get { return y; }
+		set { y = value; }
+	}
+
+	public string TenToaDo
+	{
+		get { return tenToaDo; }
+		set { tenToaDo = value; }
+	}
+
+	public override string ToString()
+	{
+		return $"{tenToaDo} ({x}, {y})";
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		ToaDo toaDo1 = new ToaDo(3.5, 2.0, "A");
+		ToaDo toaDo2 = new ToaDo(-1.0, 4.2, "B");
+		ToaDo toaDo3 = new ToaDo();
+
+		Console.WriteLine("Thông tin các tọa độ:");
+		Console.WriteLine(toaDo1);
+		Console.WriteLine(toaDo2);
+		Console.WriteLine(toaDo3);
+		Console.ReadKey();
+	}
+}
---
+++ ConsoleApp1/Bai3.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+
+class HinhTamGiac
+{
+	private double ma;
+	private double mb;
+	private double mc;
+	private string kieuTamGiac;
+
+	public HinhTamGiac()
+	{
+		ma = 0;
+		mb = 0;
+		mc = 0;
+		kieuTamGiac = "Không phải";
+	}
+
+	public HinhTamGiac(double ma, double mb, double mc)
+	{
+		if (ma <= 0 || mb <= 0 || mc <= 0 || ma + mb <= mc || ma + mc <= mb || mb + mc <= ma)
+		{
+			this.ma = 0;
+			this.mb = 0;
+			this.mc = 0;
+			kieuTamGiac = "Không phải ";
+		}
+		else
+		{
+			this.ma = ma;
+			this.mb = mb;
+			this.mc = mc;
+			KiemTraKieuTamGiac();
+		}
+	}
+
+	public double Ma
+	{
+		get { return ma; }
+		set
+		{
+			if (value > 0)
+			{
+				ma = value;
+				KiemTraKieuTamGiac();
+			}
+		}
+	}
+
+	public double Mb
+	{
+		get { return mb; }
+		set
+		{
+			if (value > 0)
+			{
+				mb = value;
+				KiemTraKieuTamGiac();
+			}
+		}
+	}
+
+	public double Mc
+	{
+		get { return mc; }
+		set
+		{
+			if (value > 0)
+			{
+				mc = value;
+				KiemTraKieuTamGiac();
+			}
+		}
+	}
+
+	private void KiemTraKieuTamGiac()
+	{
+		if (ma == mb && mb == mc)
+			kieuTamGiac = "Đều";
+		else if (ma == mb || mb == mc || ma == mc)
+			kieuTamGiac = "Cân";
+		else
+			kieuTamGiac = "Thường";
+	}
+
+	public double TinhChuVi()
+	{
+		return ma + mb + mc;
+	}
+
+	public double TinhDienTich()
+	{
+		double p = TinhChuVi() / 2;
+		return Math.Sqrt(p * (p - ma) * (p - mb) * (p - mc));
+	}
+
+	public override string ToString()
+	{
+		return $"{ma,-10:0.00} {mb,-10:0.00} {mc,-10:0.00} {kieuTamGiac,10} {TinhChuVi(),17:0.00} {TinhDienTich(),19:0.00}";
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+		Console.BackgroundColor = ConsoleColor.Cyan;
+		Console.ForegroundColor = ConsoleColor.Red;
+		HinhTamGiac tg1 = new HinhTamGiac(2, 3, 4);
+		HinhTamGiac tg2 = new HinhTamGiac(1, 2, 5);
+		HinhTamGiac tg3 = new HinhTamGiac(4, 4, 4);
+		HinhTamGiac tg4 = new HinhTamGiac(3, 4, 5);
+		HinhTamGiac tg5 = new HinhTamGiac(6, 6, 8);
+
+		Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,10} {4,17} {5,19}","Cạnh ma", "Cạnh mb", "Cạnh mc", "Kiểu TG", "Chu vi", "Diện tích");
+		Console.WriteLine(tg1);
+		Console.WriteLine(tg2);
+		Console.WriteLine(tg3);
+		Console.WriteLine(tg4);
+		Console.WriteLine(tg5);
+	}
+}
